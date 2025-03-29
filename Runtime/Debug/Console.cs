91ddb78d85f909a1eb45f6a446045ae7db3d54e6
AntrelL <77912651+AntrelL@users.noreply.github.com>
2025-03-29 03:14:45 +0300

    feat(debug): add console

---
+++ Runtime/Debug/Console.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Essentia
+{
+	public static class Console
+	{
+		private const string DebugModuleName = "Debug";
+		private const string NamespaceSeparationSymbol = ".";
+		private const string EditorPartOfNamespace = NamespaceSeparationSymbol + "Editor";
+		private const string MissingModuleNameError = "Unable to get module name if namespace is missing.";
+
+		public static void Log(object @object, string moduleName = null, string typeName = null) =>
+			Debug.Log(CreateCompositeMessage(@object.ToString(), moduleName, typeName));
+
+		public static void Log(string message, string moduleName = null, string typeName = null) =>
+			Debug.Log(CreateCompositeMessage(message, moduleName, typeName));
+
+		public static void LogWarning(string message, string moduleName = null, string typeName = null) =>
+			Debug.LogWarning(CreateCompositeMessage(message, moduleName, typeName));
+
+		public static void LogError(string message, string moduleName = null, string typeName = null) =>
+			Debug.LogError(CreateCompositeMessage(message, moduleName, typeName));
+
+		public static void Log<T>(object @object, bool isModuleName = true, bool isTypeName = true) =>
+			Debug.Log(CreateCompositeMessage<T>(@object.ToString(), isModuleName, isTypeName));
+
+		public static void Log<T>(string message, bool isModuleName = true, bool isTypeName = true) =>
+			Debug.Log(CreateCompositeMessage<T>(message, isModuleName, isTypeName));
+
+		public static void LogWarning<T>(string message, bool isModuleName = true, bool isTypeName = true) =>
+			Debug.LogWarning(CreateCompositeMessage<T>(message, isModuleName, isTypeName));
+
+		public static void LogError<T>(string message, bool isModuleName = true, bool isTypeName = true) =>
+			Debug.LogError(CreateCompositeMessage<T>(message, isModuleName, isTypeName));
+
+		private static string CreateCompositeMessage<T>(string message, bool isModuleName, bool isTypeName)
+		{
+			Type type = typeof(T);
+			string typeName = isTypeName ? type.Name : null;
+			string moduleName = isModuleName ? GetModuleName(type) : null;
+
+			return CreateCompositeMessage(message, moduleName, typeName);
+		}
+
+		private static string CreateCompositeMessage(string message, string moduleName, string typeName)
+		{
+			bool isModuleName = string.IsNullOrEmpty(moduleName) == false;
+			bool isTypeName = string.IsNullOrEmpty(typeName) == false;
+
+			if ((isModuleName || isTypeName) == false)
+				return message;
+
+			StringBuilder completedMessage = new();
+
+			completedMessage.Append(isModuleName ? $"[{moduleName}]" : string.Empty);
+			completedMessage.Append(isTypeName ? " " + typeName : string.Empty);
+
+			completedMessage.Append(completedMessage.Length > 0 ? ": " : string.Empty);
+			completedMessage.Append(message);
+
+			return completedMessage.ToString().Trim();
+		}
+
+		public static string GetModuleName(Type type)
+		{
+			string name = type.Namespace
+				?.Remove(EditorPartOfNamespace)
+				.Split(NamespaceSeparationSymbol)
+				.Last();
+
+			if (name is null)
+				LogError(MissingModuleNameError, DebugModuleName, nameof(Console));
+
+			return name;
+		}
+	}
+}
